4d8de7622d55c35dbb08fb9dabd272e18c103b06
Akshatsah04 <iamakshatsah@gmail.com>
2026-03-13 15:03:59 +0530

    jewels update

--- src/data/battleLog.js
+++ src/data/battleLog.js
@@ -1,4 +1,5 @@
 export const battleLogData = [
-  { id: 1, time: '10:12', type: 'Round Start', event: 'Round 1 started' },
+  { id: 1, time: '2:25', type: 'Round Start', event: 'Round 1 started' },
+  { id: 2, time: '2:35', type: 'Round Start', event: 'Round 2 started' },
 
 ]
